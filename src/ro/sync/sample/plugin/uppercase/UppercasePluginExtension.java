085c9a433ddca117a73663e2769be364790115ea
Alex Thomas <athom09@emory.edu>
2012-10-01 16:58:18 -0400

    A little reorg of the code and added some comments

--- src/ro/sync/sample/plugin/uppercase/UppercasePluginExtension.java
+++ src/ro/sync/sample/plugin/uppercase/UppercasePluginExtension.java
@@ -13,7 +13,7 @@ import java.lang.StringBuffer;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashMap;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONArray;
@@ -21,13 +21,14 @@ import org.json.simple.JSONArray;
 
 public class UppercasePluginExtension implements SelectionPluginExtension {
     /**
-    * Convert the text to uppercase.
+    * Lookup name in name authority.
     *
     *@param  context  Selection context.
-    *@return          Uppercase plugin result.
+    *@return          NameDropper plugin result.
     */
     public SelectionPluginResult process(SelectionPluginContext context) {
         
+        //query VIAF for name data
         String orig = context.getSelection();
         String result = this.queryVIAF(orig);
         
@@ -36,23 +37,79 @@ public class UppercasePluginExtension implements SelectionPluginExtension {
     
     
     /**
-    * Convert the text to uppercase.
+    * Query VIAF for name data
     *
-    *@param  context  Selection context.
-    *@return          Uppercase plugin result.
+    *@param  name  Name to query.
+    *@return          String containing persname xml tag data.
     */
-    public String queryVIAF(String param){
-        String result = param; //if something goes boom it will put back the original text
+    public String queryVIAF(String name){
+        String result = name;  //This is retutned if no resulsts are found
+
+        // url paramters
+        HashMap  params = new HashMap();
+        params.put("query", name);
         
+        
+        // get the result of the query
+        String query_result = this.query("http://viaf.org/viaf/AutoSuggest", params);
+        
+        
+        // parse the JSON and return resut in the correct format
         try{
-            StringBuffer url_buf = new StringBuffer();
-            url_buf.append("http://viaf.org/viaf/AutoSuggest?");
-            url_buf.append("query=");
-            url_buf.append(URLEncoder.encode(param, "UTF-8"));
+            JSONObject json = (JSONObject)new JSONParser().parse(query_result);
+            JSONArray json_array = (JSONArray)json.get("result");
+            JSONObject obj = (JSONObject) json_array.get(0);
+            String viafid = (String)obj.get("viafid");
+//            
+            result = "<persname source=\"viaf\" authfilenumber=\"" + viafid + "\">" + name + "</persname>";
+        }catch(Exception e){}               
+        
+        return result;
+    
+    
+    }
+    
+    /**
+    * performs a query against the provided url using the provided query parms.
+    *
+    *@param  ulr  Base url to query.
+    *@param  params   key value pairs of query parameters.
+    *@return          Results of query.
+    */
+    public String query(String url, HashMap params){
+        String result = (String)params.get("query");  //Used if there are no results
+        StringBuffer url_buf = new StringBuffer();
+        url_buf.append(url);
+        
+       //Build query string
+        for (int i=0; i < params.size(); i++) {
+            String key = (String)params.keySet().toArray()[i];
+            String val = (String)params.values().toArray()[i];
             
-            URL url = new URL(url_buf.toString());
+            
+            //put the ?  if params exits
+            if(i==0 && !url.endsWith("?")){
+                url_buf.append("?");
+            }
+            
+            
+            //Append the key, value pairs
+            try{
+                url_buf.append(key + "=" + URLEncoder.encode(val, "UTF-8"));    
+            }catch(Exception e){}
+            
+            
+            if(i != params.size());{
+                url_buf.append("&");
+            }
+
+        }
+        
+        // Do the acual query
+        try{
+            URL url_obj = new URL(url_buf.toString());
             HttpURLConnection connection = null;
-            connection = (HttpURLConnection)url.openConnection();
+            connection = (HttpURLConnection)url_obj.openConnection();
             connection.setDoOutput(true);
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder sb = new StringBuilder();
@@ -61,19 +118,10 @@ public class UppercasePluginExtension implements SelectionPluginExtension {
                 sb.append(line+"\n");
             }
             br.close();
+            result = sb.toString();
             
-            JSONObject json = (JSONObject)new JSONParser().parse(sb.toString());
-            JSONArray json_array = (JSONArray)json.get("result");
-            JSONObject obj = (JSONObject) json_array.get(0);
-            String viafid = (String)obj.get("viafid");
-//            
-            result = "<persname source=\"viaf\" authfilenumber=\"" + viafid + "\">" + param + "</persname>";
-        }catch(Exception e){
-            result = e.getMessage();
-        }               
+        }catch(Exception e){}  //No action yet on Exception
         
         return result;
-    
-    
     }
 }
