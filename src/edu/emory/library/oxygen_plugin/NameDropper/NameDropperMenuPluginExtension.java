4d5999ea2e71f3e4ec52d89c5ad4484a0f47abf5
Alex Thomas <athom09@emory.edu>
2012-10-19 12:49:16 -0400

    [#Finishes #38095729] Revised menu functions to gray out currently selected option

--- src/edu/emory/library/oxygen_plugin/NameDropper/NameDropperMenuPluginExtension.java
+++ src/edu/emory/library/oxygen_plugin/NameDropper/NameDropperMenuPluginExtension.java
@@ -19,17 +19,15 @@
 package edu.emory.library.oxygen_plugin.NameDropper;
 
 import java.awt.event.ActionEvent;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JTextArea;
 import ro.sync.exml.plugin.workspace.WorkspaceAccessPluginExtension;
+import ro.sync.exml.workspace.api.options.WSOptionChangedEvent;
+import ro.sync.exml.workspace.api.options.WSOptionListener;
 import ro.sync.exml.workspace.api.standalone.MenuBarCustomizer;
 import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
 import ro.sync.exml.workspace.api.standalone.ui.Menu;
@@ -38,15 +36,6 @@ import ro.sync.exml.workspace.api.standalone.ui.Menu;
  * Plugin extension - workspace access extension.
  */
 public class NameDropperMenuPluginExtension implements WorkspaceAccessPluginExtension {
-
-  /**
-   * Plugin workspace access.
-   */
-  private StandalonePluginWorkspace pluginWorkspaceAccess;
-  
-  public void applicationStarted(final StandalonePluginWorkspace pluginWorkspaceAccess) {
-    this.pluginWorkspaceAccess = pluginWorkspaceAccess;
-    
     // Set EAD action
     final Action setEADAction = new AbstractAction() {
       public void actionPerformed(ActionEvent arg0) { 
@@ -61,48 +50,77 @@ public class NameDropperMenuPluginExtension implements WorkspaceAccessPluginExte
       }
     };
     
+    //Option Listener - triggers when docType option changes
+    WSOptionListener OL = new WSOptionListener("docType"){
+        @Override
+        public void  optionValueChanged(WSOptionChangedEvent e) {
+            String optVal = e.getNewValue();
+            if(optVal.equals("EAD")){
+                setEADAction.setEnabled(false);
+                setTEIAction.setEnabled(true);
+            }
+            
+            if(optVal.equals("TEI")){
+                setTEIAction.setEnabled(false);
+                setEADAction.setEnabled(true);
+            }
+        }
+    };    
+
+  /**
+   * Plugin workspace access.
+   */
+  private StandalonePluginWorkspace pluginWorkspaceAccess;
+    
+  public void applicationStarted(final StandalonePluginWorkspace pluginWorkspaceAccess) {
+    this.pluginWorkspaceAccess = pluginWorkspaceAccess;
+    
     pluginWorkspaceAccess.addMenuBarCustomizer(new MenuBarCustomizer() { 
 
       public void customizeMainMenu(JMenuBar mainMenuBar) {
         // nameDropper menu
-        JMenu ndMenu = createNDMenu(setEADAction, setTEIAction);
+        JMenu ndMenu = createNDMenu();
         // Add the ndMenu
-        mainMenuBar.add(ndMenu, mainMenuBar.getMenuCount() - 1);
-        
+        mainMenuBar.add(ndMenu, mainMenuBar.getMenuCount() - 1);        
       }
     });
-
+    
+    // Attach option Listner to the options
+    pluginWorkspaceAccess.getOptionsStorage().addOptionListener(OL);
  }
   
   /**
    * Create menu that contains the following actions:
-   * setEAD, setTEI
-   * 
-   * @param setEADAction Sets docType prop to EAD.
-   * @param setTEIAction Sets docType prop to TEI.
-   
-   * 
+   * setEAD, setTEI 
    * @return The ndMenu.
    */
-  private JMenu createNDMenu(
-      final Action setEAD, 
-      final Action setTEI) {
+  private JMenu createNDMenu() {      
     // ndMenu
     Menu ndMenu = new Menu("NameDropper", true); 
     
     // Add setEAD action on the menu
-    final JMenuItem setEADItem = new JMenuItem(setEAD); 
+    final JMenuItem setEADItem = new JMenuItem(setEADAction); 
     setEADItem.setText("Set EAD");
     ndMenu.add(setEADItem);
 
     // Add setTEI action on the menu
-    final JMenuItem setTEIItem = new JMenuItem(setTEI); 
+    final JMenuItem setTEIItem = new JMenuItem(setTEIAction); 
     setTEIItem.setText("Set TEI");
     ndMenu.add(setTEIItem);
+    
+    if(pluginWorkspaceAccess.getOptionsStorage().getOption("docType", "").equals("EAD")) {
+        setEADAction.setEnabled(false);
+        setTEIAction.setEnabled(true);
+    }
+    
+    if(pluginWorkspaceAccess.getOptionsStorage().getOption("docType", "").equals("TEI")) {
+        setTEIAction.setEnabled(false);
+        setEADAction.setEnabled(true);
+    }
     
     return ndMenu;
   }
 
   //aparently required for some reason
-  public boolean applicationClosing() {return true;}
+  public boolean applicationClosing() {return true;} 
 }
