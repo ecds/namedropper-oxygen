f5b36fba5196ef2ee752db18ae7634a8c0a728ba
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-11-21 19:49:22 -0500

    preliminary enum for document type info; use radio menu items in doctype menu

---
+++ src/edu/emory/library/namedropper/plugins/DocumentType.java
@@ -0,0 +1,30 @@
+/**
+ * file src/edu/emory/library/namedropper/plugins/NameDropperMenuPlugin.java
+ *
+ * Copyright 2012 Emory University Library
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.emory.library.namedropper.plugins;
+
+
+/**
+ * Information about supported document types.
+ */
+public enum DocumentType {
+    EAD,
+    TEI;
+
+    // TODO: shift tag name and attribute details here, if possible
+}
