a3a7113b6c56aaec3a68a613b45d886ffd276397
rlskoeser <rebecca.s.koeser@emory.edu>
2013-02-15 16:46:25 -0500

    preliminary/partial unit tests for geonames lookup

---
+++ src/edu/emory/library/namedropper/tests/SelectionActionGeoNamesTest.java
@@ -0,0 +1,122 @@
+/**
+ * file src/edu/emory/library/namedropper/tests/SelectionActionGeoNamesTest.java
+ *
+ * Copyright 2012 Emory University Library
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package edu.emory.library.namedropper.tests;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
+
+import org.geonames.WebService;
+import org.geonames.Toponym;
+import org.geonames.ToponymSearchCriteria;
+import org.geonames.ToponymSearchResult;
+
+import edu.emory.library.namedropper.plugins.DocumentType;
+import edu.emory.library.namedropper.plugins.SelectionActionGeoNames;
+import edu.emory.library.namedropper.plugins.PluginOptions;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PluginOptions.class, WebService.class})
+public class SelectionActionGeoNamesTest {
+    SelectionActionGeoNames action;
+
+    @Before
+    public void setUp() {
+        // initialize mock action
+        this.action = Mockito.mock(SelectionActionGeoNames.class);
+        //this.action = new SelectionActionGeoNames((StandalonePluginWorkspace)PluginOptions.getWorkspace());
+    }
+
+    @After
+    public void tearDown() {
+        this.action = null;
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testNoDoctype() throws Exception {
+        String term = "Belfast";
+        Mockito.when(this.action.queryGeoNames(term)).thenCallRealMethod();
+        // no doctype
+        assertEquals(null, this.action.queryGeoNames(term));
+    }
+
+    @Test
+    public void testQueryNoUsername() throws Exception {
+        PowerMockito.mockStatic(PluginOptions.class);
+        exception.expect(Exception.class);
+        exception.expectMessage("Please set a GeoNames.org username");
+
+        // make sure username option is empty
+        //Mockito.when(SelectionActionGeoNames.getGeoNamesUsername()).thenReturn("");
+        Mockito.when(PluginOptions.getOption(SelectionActionGeoNames.GEONAMES_USERNAME)).thenReturn("");
+        String term = "Belfast";
+        Mockito.when(this.action.queryGeoNames(term)).thenCallRealMethod();
+        // doctype must be set
+        this.action.docType = DocumentType.EAD;
+        this.action.queryGeoNames(term);
+    }
+
+    @Test
+    public void testQueryNoResults() throws Exception {
+        PowerMockito.mockStatic(PluginOptions.class);
+        PowerMockito.mockStatic(WebService.class);
+        exception.expect(Exception.class);
+        exception.expectMessage("No Results");
+
+        String term = "Belfast";
+        String username = "user";
+        // make sure username option is not empty
+        Mockito.when(PluginOptions.getOption(SelectionActionGeoNames.GEONAMES_USERNAME)).thenReturn(username);
+        // set mocked service to return an empty search result
+        ToponymSearchResult emptyResult = new ToponymSearchResult();
+        emptyResult.setTotalResultsCount(0);
+        Mockito.when(WebService.search((ToponymSearchCriteria)Mockito.anyObject())).thenReturn(emptyResult);
+
+        Mockito.when(this.action.queryGeoNames(term)).thenCallRealMethod();
+        // doctype must still be set
+        this.action.docType = DocumentType.EAD;
+        this.action.queryGeoNames(term);
+        PowerMockito.verifyStatic(); //WebService.class);
+        WebService.setUserName(username);
+    }
+
+
+}
